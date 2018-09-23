042e11d6955c2326c5f4b69c0f39ee1b6125ba41
ladyada <limor@ladyada.net>
2018-09-22 22:53:27 -0400

    init

---
+++ examples/mprls_simpletest/mprls_simpletest.ino
@@ -0,0 +1,45 @@
+/*!
+ * @file mprls_simpletest.ino
+ *
+ * A basic test of the sensor with default settings
+ * 
+ * Designed specifically to work with the MPRLS sensor from Adafruit
+ * ----> https://www.adafruit.com/products/3965
+ *
+ * These sensors use I2C to communicate, 2 pins (SCL+SDA) are required
+ * to interface with the breakout.
+ *
+ * Adafruit invests time and resources providing this open source code,
+ * please support Adafruit and open-source hardware by purchasing
+ * products from Adafruit!
+ *
+ * Written by Limor Fried/Ladyada for Adafruit Industries.  
+ *
+ * MIT license, all text here must be included in any redistribution.
+ *
+ */
+ 
+#include <Wire.h>
+#include "Adafruit_MPRLS.h"
+
+// You dont *need* a reset and EOC pin for most uses, so we set to -1 and don't connect
+#define RESET_PIN  -1  // set to any GPIO pin # to hard-reset on begin()
+#define EOC_PIN    -1  // set to any GPIO pin to read end-of-conversion by pin
+Adafruit_MPRLS mpr = Adafruit_MPRLS(RESET_PIN, EOC_PIN);
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("MPRLS Simple Test");
+  if (! mpr.begin()) {
+    Serial.println("Failed to communicate with MPRLS sensor, check wiring?");
+  }
+  Serial.println("Found MPRLS sensor");
+}
+
+
+void loop() {
+  float pressure_hPa = mpr.readPressure();
+  Serial.print("Pressure (hPa): "); Serial.println(pressure_hPa);
+  Serial.print("Pressure (PSI): "); Serial.println(pressure_hPa / 68.947572932);
+  delay(1000);
+}
